af6e7d86f3c2c6d131c50a83bd799b3b22e2463f
Senghor LONTSI <senghorguieto@gmail.com>
2025-05-27 12:33:18 +0200

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 
     //Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.56.2")
-    //kapt("com.google.dagger:hilt-android-compiler:2.56.2")
+    /*kapt("com.google.dagger:hilt-android-compiler:2.56.2")*/
     implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
 
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
